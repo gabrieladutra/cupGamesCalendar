bc11ffa8aa692ea343016896ddb0923e70461db5
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-05 18:52:08 -0300

    WIP

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ function createGame(player1, hour, player2) {
 `
 }
 
+let delay = -0.4;
+
 function createCard(date, day, games) {
+    delay = delay +
+        0.4;
     return `
-		<div class="card">
+		<div class="card" stle"animation-delay: ${delay}s">
                 <h2>${date}
                     <span>${day}</span>
                 </h2>
